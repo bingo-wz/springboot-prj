600fa0ce93f6ae289443b03de1816e69e1bd1193
bingo <bingo@qq.com>
2022-09-18 00:28:54 +0800

    test:第一次成功用springboot运行web网页并成功！

---
+++ 02-springboot-frist/src/main/java/com/bingo/controller/HelloSpringBoot.java
@@ -0,0 +1,17 @@
+package com.bingo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloSpringBoot {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String helloSpringBoot(){
+        return "欢迎使用spring boot框架";
+    }
+
+}
