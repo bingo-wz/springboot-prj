ee9c7550558f1875cbd23b1133c53332ae5156a1
bingo <bingo@qq.com>
2022-09-18 17:45:51 +0800

    多环境配置:有开发环境， 测试环境， 上线的环境

--- 02-springboot-frist/src/main/java/com/bingo/controller/HelloSpringBoot.java
+++ 02-springboot-frist/src/main/java/com/bingo/controller/HelloSpringBoot.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloSpringBoot {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     @ResponseBody
     public String helloSpringBoot(){
         return "欢迎使用spring boot框架,设置了端口";
