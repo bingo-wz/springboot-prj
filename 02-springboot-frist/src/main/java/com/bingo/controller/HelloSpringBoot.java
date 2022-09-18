91abebf9ace3716225d1140156eff6bc0d35a7fb
bingo <bingo@qq.com>
2022-09-18 19:35:16 +0800

    @Value来读取配置文件中的值

--- 02-springboot-frist/src/main/java/com/bingo/controller/HelloSpringBoot.java
+++ 02-springboot-frist/src/main/java/com/bingo/controller/HelloSpringBoot.java
@@ -1,5 +1,6 @@
 package com.bingo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloSpringBoot {
 
+    @Value("${server.port}")
+    private Integer port;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Value("${school.name}")
+    private String name;
+
+    @Value("${site}")
+    private String site;
+
     @RequestMapping("/")
     @ResponseBody
     public String helloSpringBoot(){
-        return "欢迎使用spring boot框架,设置了端口";
+        return name+",site="+site+"项目的访问地址："+contextPath+"，使用的端口："+port;
     }
-
 }
